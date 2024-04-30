424249e295ee7538d3d868f03f88f112df094aae
kyle <kadelcastillo@up.edu.ph>
2024-05-01 06:22:00 +0800

    changed name due to flask

--- app.py
+++ app.py
@@ -1,8 +1,9 @@
 import secrets
 import sqlite3
 
-from flask import Flask, request, render_template, redirect
-
+from flask import Flask, make_response, request, render_template, redirect, jsonify
+from helper import sanitize, is_rate_limited
+import datetime
 
 app = Flask(__name__)
 con = sqlite3.connect("app.db", check_same_thread=False)
@@ -12,24 +13,31 @@ def login():
     cur = con.cursor()
     if request.method == "GET":
         if request.cookies.get("session_token"):
+            # parameteriize session tokens
             res = cur.execute("SELECT username FROM users INNER JOIN sessions ON "
-                              + "users.id = sessions.user WHERE sessions.token = '"
-                              + request.cookies.get("session_token") + "'")
+                              + "users.id = sessions.user WHERE sessions.token = ?", [request.cookies.get("session_token")])
             user = res.fetchone()
+            
             if user:
                 return redirect("/home")
 
         return render_template("login.html")
     else:
-        res = cur.execute("SELECT id from users WHERE username = '"
-                    + request.form["username"]
-                    + "' AND password = '"
-                    + request.form["password"] + "'")
+        remote_address = request.remote_addr
+        cur.execute("INSERT INTO login_attempts (remote_address, timestamp) VALUES(?,?)", [remote_address, datetime.datetime.now()])
+        con.commit()
+        
+        if is_rate_limited(cur, remote_address):
+            return render_template("login.html", error="Login attempts exceeded limit.")
+        
+        # parameterized username and password to prevent SQL injection
+        res = cur.execute("SELECT id from users WHERE username = ? AND password = ?", [request.form["username"], request.form["password"]])
         user = res.fetchone()
         if user:
             token = secrets.token_hex()
-            cur.execute("INSERT INTO sessions (user, token) VALUES ("
-                        + str(user[0]) + ", '" + token + "');")
+            
+            # parameterize username and token 
+            cur.execute("INSERT INTO sessions (user, token) VALUES (?, ?);", [str(user[0]), token])
             con.commit()
             response = redirect("/home")
             response.set_cookie("session_token", token)
@@ -42,14 +50,21 @@ def login():
 def home():
     cur = con.cursor()
     if request.cookies.get("session_token"):
+        
+        # parameterize session token
         res = cur.execute("SELECT users.id, username FROM users INNER JOIN sessions ON "
-                          + "users.id = sessions.user WHERE sessions.token = '"
-                          + request.cookies.get("session_token") + "';")
+                          + "users.id = sessions.user WHERE sessions.token = ?;", [request.cookies.get("session_token")])
         user = res.fetchone()
         if user:
-            res = cur.execute("SELECT message FROM posts WHERE user = " + str(user[0]) + ";")
+            # Generate CSRF Token
+            csrf_token = secrets.token_urlsafe(16)
+
+            res = cur.execute("SELECT message FROM posts WHERE user ='" + str(user[0]) + "';")
             posts = res.fetchall()
-            return render_template("home.html", username=user[1], posts=posts)
+
+            resp = make_response(render_template("home.html", username=user[1], posts=posts, csrf_token=csrf_token))
+            resp.set_cookie("csrf_token", csrf_token, httponly=True)
+            return resp
 
     return redirect("/login")
 
@@ -57,18 +72,16 @@ def home():
 @app.route("/posts", methods=["POST"])
 def posts():
     cur = con.cursor()
-    if request.cookies.get("session_token"):
+    if request.cookies.get("session_token") and request.form.get("csrf_token") == request.cookies.get("csrf_token"):
         res = cur.execute("SELECT users.id, username FROM users INNER JOIN sessions ON "
-                          + "users.id = sessions.user WHERE sessions.token = '"
-                          + request.cookies.get("session_token") + "';")
+                          + "users.id = sessions.user WHERE sessions.token = ?;", [request.cookies.get("session_token")])
         user = res.fetchone()
         if user:
-            cur.execute("INSERT INTO posts (message, user) VALUES ('"
-                        + request.form["message"] + "', " + str(user[0]) + ");")
+            cur.execute("INSERT INTO posts (message, user) VALUES (?,?);", [sanitize(request.form["message"]), str(user[0])])
             con.commit()
             return redirect("/home")
 
-    return redirect("/login", error="test")
+    return redirect("/login")
 
 
 @app.route("/logout", methods=["GET"])
@@ -76,11 +89,10 @@ def logout():
     cur = con.cursor()
     if request.cookies.get("session_token"):
         res = cur.execute("SELECT users.id, username FROM users INNER JOIN sessions ON "
-                          + "users.id = sessions.user WHERE sessions.token = '"
-                          + request.cookies.get("session_token") + "'")
+                          + "users.id = sessions.user WHERE sessions.token = ?", [request.cookies.get("session_token")])
         user = res.fetchone()
         if user:
-            cur.execute("DELETE FROM sessions WHERE user = " + str(user[0]) + ";")
+            cur.execute("DELETE FROM sessions WHERE user = ?;", [str(user[0])])
             con.commit()
 
     response = redirect("/login")
